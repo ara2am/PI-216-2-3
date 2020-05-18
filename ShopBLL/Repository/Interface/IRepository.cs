a70723eb7e72e86bb0a02f39745df1cf1e25ff55
Remez Pavel <github.aidecay@gmail.com>
2020-05-19 01:08:41 +0300

    +Add Template Interaface for Repository

---
+++ ShopBLL/Repository/Interface/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopBLL.Repository.Interface
+{
+    interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T Get(int Id);
+        void Add(T obj);
+        void Update(T obj);
+        void Del(int Id);
+    }
+}

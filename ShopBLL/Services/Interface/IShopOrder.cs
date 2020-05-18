927ce08cd1f9b214b9c0478f71d0b27050bd5896
Remez Pavel <github.aidecay@gmail.com>
2020-05-18 19:28:03 +0300

    +Add services for ShopOrder

---
+++ ShopBLL/Services/Interface/IShopOrder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ShopBLL.Entity.ShopItem.ShopGoods;
+using ShopBLL.Entity.ShopItem.ShopUtility.ShopEnums;
+
+namespace ShopBLL.Entity.Interface
+{
+    interface IShopOrder
+    {
+        void UpdateDeliveryState(ShopOrderDeliveryState state);
+        void UpdatePaymentState(ShopOrderPaymentState state);
+    }
+}

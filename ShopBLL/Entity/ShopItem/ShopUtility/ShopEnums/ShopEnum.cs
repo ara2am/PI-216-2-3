6c783250d3fabc56f947faae405f841731446b72
Remez Pavel <github.aidecay@gmail.com>
2020-05-18 19:30:23 +0300

    +Add ShopEnum (State)

---
+++ ShopBLL/Entity/ShopItem/ShopUtility/ShopEnums/ShopEnum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopBLL.Entity.ShopItem.ShopUtility.ShopEnums
+{
+    public enum ShopOrderDeliveryState
+    {
+        Delivered = 0,
+        In_Delivery = 1,
+        Awaiting_delivery = 2,
+    }
+
+    public enum ShopOrderPaymentState
+    {
+        Paid = 0,
+        Not_paid = 1,
+        Partially_paid = 2,
+    }
+}

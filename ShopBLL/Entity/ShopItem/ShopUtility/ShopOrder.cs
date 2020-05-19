9eba753b635e732d5207e90c4c423191237eeebb
Remez Pavel <github.aidecay@gmail.com>
2020-05-19 20:40:16 +0300

    *Ovveride abstr field

--- ShopBLL/Entity/ShopItem/ShopUtility/ShopOrder.cs
+++ ShopBLL/Entity/ShopItem/ShopUtility/ShopOrder.cs
@@ -3,14 +3,18 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using ShopBLL.Entity.ShopItem.ShopUtility.ShopEnums;
+
 namespace ShopBLL.Entity.ShopItem.ShopGoods
 {
     class ShopOrder : Order
     {
+        public override int OrderId { get; set; }
+        public override DateTime Time { get; set; }
+        public override double Price { get; set; }
         public ShopOrderDeliveryState DeliveryState { get; set; }
         public ShopOrderPaymentState PaymentState { get; set; }
         public List<Goods> ListOfGoods { get; set; }
-
+        
         public ShopOrder()
         {
             ListOfGoods = new List<Goods>();

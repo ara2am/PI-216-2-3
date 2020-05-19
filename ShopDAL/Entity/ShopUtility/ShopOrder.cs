68b697e5e61882de2e6582e9cfe9524f99263db0
Remez Pavel <github.aidecay@gmail.com>
2020-05-19 20:38:54 +0300

    +Add entity ShopOrder to DAL (with annotations)

---
+++ ShopDAL/Entity/ShopUtility/ShopOrder.cs
@@ -0,0 +1,21 @@
+﻿using ShopDAL.Entity.ShopItem.ShopEnums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopDAL.Entity.ShopUtility
+{
+    class ShopOrder : Order
+    {
+        public ShopOrderDeliveryState DeliveryState { get; set; }
+        public ShopOrderPaymentState PaymentState { get; set; }
+        public List<Goods> ListOfGoods { get; set; }
+
+        public ShopOrder()
+        {
+            ListOfGoods = new List<Goods>();
+        }
+    }
+}

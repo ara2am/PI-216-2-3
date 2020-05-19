16a8cc92714a5d308f6daef2e75ed1beaf6e525a
Remez Pavel <github.aidecay@gmail.com>
2020-05-19 20:34:53 +0300

    +Add dbcontext for shop

---
+++ ShopDAL/Context/ContextClasses/ShopContext.cs
@@ -0,0 +1,15 @@
+﻿using ShopDAL.Context.Interface;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopDAL.Context.ContextClasses
+{
+    class ShopContext : DbContext, IShopContext
+    {
+
+    }
+}

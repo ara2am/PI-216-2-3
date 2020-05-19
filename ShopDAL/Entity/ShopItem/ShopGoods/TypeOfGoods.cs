c91c5e22b4e4196d802d9eeba85d60473bbbc62a
Remez Pavel <github.aidecay@gmail.com>
2020-05-19 20:37:41 +0300

    +Add entity typeofgoods to DAL (with annotations)

---
+++ ShopDAL/Entity/ShopItem/ShopGoods/TypeOfGoods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopDAL.Entity.ShopItem.ShopEnums
+{
+    class TypeOfGoods
+    {
+        [Key]
+        public int TypeOfGoodsId { get; set; }
+
+        [Required]
+        [StringLength(250, MinimumLength = 5, ErrorMessage = "Name must be between 5 and 250 characters")]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(1, 200, ErrorMessage = "Age must be in interval [5,250]")]
+        public int AgeLimit { get; set; }
+        public bool NeedPermission { get; set; }
+    }
+}

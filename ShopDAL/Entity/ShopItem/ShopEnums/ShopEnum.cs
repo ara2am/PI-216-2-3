64f4c9c77ad6fdbf22973fc97a5b671eb6bf155b
Remez Pavel <github.aidecay@gmail.com>
2020-05-19 20:36:14 +0300

    +Add enum to DAL

---
+++ ShopDAL/Entity/ShopItem/ShopEnums/ShopEnum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopDAL.Entity.ShopItem.ShopEnums
+{
+    public enum ShopOrderDeliveryState
+    {
+        Delivered = 0,
+        In_Delivery = 1,
+        Awaiting_delivery = 2,
+    }
+
+    public enum ShopOrderPaymentState
+    {
+        Paid = 0,
+        Not_paid = 1,
+        Partially_paid = 2,
+    }
+}

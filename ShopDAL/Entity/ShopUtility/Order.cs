9a8df82bb483a617b09279cb156b7d7e2e6a3f82
Remez Pavel <github.aidecay@gmail.com>
2020-05-19 20:38:29 +0300

    +Add abstr class Order to DAL

---
+++ ShopDAL/Entity/ShopUtility/Order.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopDAL.Entity.ShopUtility
+{
+    class Order
+    {
+        [Key]
+        public override int OrderId { get; set; }
+        public override DateTime Time { get; set; }
+
+        [Required]
+        [Range(double.Epsilon, double.MaxValue, ErrorMessage = "Must be positive value")]
+        public override double Price { get; set; }
+        public ShopOrderDeliveryState DeliveryState { get; set; }
+        public ShopOrderPaymentState PaymentState { get; set; }
+        public List<Goods> ListOfGoods { get; set; }
+
+        public ShopOrder()
+        {
+            ListOfGoods = new List<Goods>();
+        }
+    }
+}

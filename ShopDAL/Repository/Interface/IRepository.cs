bd027ccf250ee3c7bdf40acc46582b0cddd57ffb
Remez Pavel <github.aidecay@gmail.com>
2020-05-20 20:00:35 +0300

    +Add template interface for Repository

---
+++ ShopDAL/Repository/Interface/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopBLL.Repository.Interface
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T Get(int Id);
+        void Add(T obj);
+        void Del(int Id);
+        void Update(T obj);
+    }
+}

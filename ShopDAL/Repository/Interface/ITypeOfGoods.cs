dc12bad04c128969f12a317a7eddc6b0968c971f
Remez Pavel <github.aidecay@gmail.com>
2020-05-20 20:05:12 +0300

    +Add interface for TypeOfGoodsRepository

---
+++ ShopDAL/Repository/Interface/ITypeOfGoods.cs
@@ -0,0 +1,14 @@
+﻿using ShopBLL.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopDAL.Repository.Interface
+{
+    public interface ITypeOfGoods : IRepository<ITypeOfGoods>
+    {
+        
+    }
+}

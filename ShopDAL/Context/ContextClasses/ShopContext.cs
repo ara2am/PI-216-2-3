aa6553d62eadd4873809228fcc171bad2f2aa475
Remez Pavel <github.aidecay@gmail.com>
2020-05-20 20:03:11 +0300

    +Add DbSet collection of entity

--- ShopDAL/Context/ContextClasses/ShopContext.cs
+++ ShopDAL/Context/ContextClasses/ShopContext.cs
@@ -1,4 +1,8 @@
-﻿using ShopDAL.Context.Interface;
+﻿using ShopDAL.Config;
+using ShopDAL.Context.Interface;
+using ShopDAL.Entity.ShopItem.ShopEnums;
+using ShopDAL.Entity.ShopItem.ShopGoods;
+using ShopDAL.Entity.ShopUtility;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -8,8 +12,25 @@ using System.Threading.Tasks;
 
 namespace ShopDAL.Context.ContextClasses
 {
-    class ShopContext : DbContext, IShopContext
+    public class ShopContext : DbContext, IShopContext
     {
+        public ShopContext()
+            : base("DbShopConnection")
+        {
 
+        }
+
+        public DbSet<ShopOrder> ShopOrders;
+
+        public DbSet<Goods> Goods;
+
+        public DbSet<TypeOfGoods> TypeOfGoods;
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new GoodsConfig());
+            modelBuilder.Configurations.Add(new ShopOrderConfig());
+            modelBuilder.Configurations.Add(new TypeOfGoodsConfig());
+        }
     }
 }

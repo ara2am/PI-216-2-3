c7e62d324e773c47c723e31319c390fec0324b26
Remez Pavel <github.aidecay@gmail.com>
2020-05-24 20:45:27 +0300

    Add repository for TypeOfGoods

---
+++ ShopDAL/Repository/RepositoryEntity/TypeOfGoodsRepository.cs
@@ -0,0 +1,25 @@
+﻿using ShopDAL.Context.ContextClasses;
+using ShopDAL.Entity.ShopItem.ShopGoods;
+using ShopDAL.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopDAL.Repository.RepositoryEntity
+{
+    public class TypeOfGoodsRepository : Repository<TypeOfGoods>, ITypeOfGoodsRepository
+    {
+        public TypeOfGoodsRepository(ShopContext context)
+            : base(context)
+        {
+
+        }
+
+        public ShopContext PContext
+        {
+            get { return Context as ShopContext; }
+        }
+    }
+}

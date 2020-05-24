929d77cc03080a4f1f0d7275a13c4374289182de
Remez Pavel <github.aidecay@gmail.com>
2020-05-24 20:45:04 +0300

    Add repository for ShopOrder

---
+++ ShopDAL/Repository/RepositoryEntity/ShopOrderRepository.cs
@@ -0,0 +1,32 @@
+﻿using ShopDAL.Context.ContextClasses;
+using ShopDAL.Entity.ShopItem.ShopGoods;
+using ShopDAL.Entity.ShopUtility;
+using ShopDAL.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopDAL.Repository.RepositoryEntity
+{
+    public class ShopOrderRepository : Repository<ShopOrder>, IShopOrderRepository 
+    {
+        public ShopOrderRepository(ShopContext context)
+            : base(context)
+        {
+
+        }
+
+        public ShopContext PContext
+        {
+            get { return Context as ShopContext; }
+        }
+
+        public IEnumerable<Goods> GetGoods(int id)
+        {
+            return Get(id).ListOfGoods;
+        }
+
+    }
+}

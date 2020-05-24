4471d3412fb6df52fd6d93a015bad467bc4c9264
Remez Pavel <github.aidecay@gmail.com>
2020-05-24 20:48:08 +0300

    *Some fix

---
+++ ShopDAL/Repository/Interface/ITypeOfGoodsRepository.cs
@@ -0,0 +1,15 @@
+﻿using ShopBLL.Repository.Interface;
+using ShopDAL.Entity.ShopItem.ShopGoods;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopDAL.Repository.Interface
+{
+    public interface ITypeOfGoodsRepository : IRepository<TypeOfGoods>
+    {
+        
+    }
+}

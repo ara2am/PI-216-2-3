0a890f6b8eab3a2e3b24a6724797d757bcb8d265
Remez Pavel <github.aidecay@gmail.com>
2020-05-24 20:44:50 +0300

    Add template class for all repository

---
+++ ShopDAL/Repository/RepositoryEntity/Repository.cs
@@ -0,0 +1,69 @@
+﻿using ShopBLL.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopDAL.Repository.RepositoryEntity
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly DbContext Context;
+
+        public Repository(DbContext context)
+        {
+            Context = context;
+        }
+
+        public T Get(int id)
+        {
+            return Context.Set<T>().Find(id);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return Context.Set<T>().ToList();
+        }
+
+        public IEnumerable<T> Find(Expression<Func<T, bool>> predicate)
+        {
+            return Context.Set<T>().Where(predicate);
+        }
+
+        public T SingleOrDefault(Expression<Func<T, bool>> predicate)
+        {
+            return Context.Set<T>().SingleOrDefault(predicate);
+        }
+
+        public void Add(T obj)
+        {
+            Context.Set<T>().Add(obj);
+        }
+
+        public void AddRange(IEnumerable<T> entities)
+        {
+            Context.Set<T>().AddRange(entities);
+        }
+
+        public void Del(T obj)
+        {
+            var entry = Context.Entry(obj);
+            entry.State = EntityState.Deleted;
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            Context.Set<T>().RemoveRange(entities);
+        }
+
+        public void Update(int id, T obj)
+        {
+            var entry = Context.Entry(Get(id));
+            entry.CurrentValues.SetValues(obj);
+            entry.State = EntityState.Modified;
+        }
+    }
+}

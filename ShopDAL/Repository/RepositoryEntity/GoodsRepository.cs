57d03001d03b412a5e5b99c64149b594a45f25e4
Remez Pavel <github.aidecay@gmail.com>
2020-05-24 20:44:25 +0300

    Add repository for Goods

---
+++ ShopDAL/Repository/RepositoryEntity/GoodsRepository.cs
@@ -0,0 +1,32 @@
+﻿using ShopDAL.Context.ContextClasses;
+using ShopDAL.Entity.ShopItem.ShopGoods;
+using ShopDAL.Entity.ShopUtility;
+using ShopDAL.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopDAL.Repository.RepositoryEntity
+{
+    public class GoodsRepository : Repository<Goods>, IGoodsRepository
+    {
+        public GoodsRepository(ShopContext context)
+            :base(context)
+        {
+
+        }
+
+        public ShopContext PContext
+        {
+            get { return Context as ShopContext; }
+        }
+
+        public IEnumerable<ShopOrder> GetOrders(int id)
+        {
+            return Get(id).ListOfOrders;
+        }
+
+    }
+}

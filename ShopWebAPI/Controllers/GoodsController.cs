5f71abb67e7000f455b1523278810b0159b50d8e
Aradam <fox@aradam.pp.ua>
2020-05-29 09:45:57 +0300

    + Realised GoodsController and Models

--- ShopWebAPI/Controllers/GoodsController.cs
+++ ShopWebAPI/Controllers/GoodsController.cs
@@ -5,32 +5,76 @@ using System.Web;
 using System.Web.Http;
 using ShopWebAPI.Models.Goods;
 using ShopBLL.Services.Interface;
+using ShopBLL.Services.ServicesEntity;
+using ShopBLL.Entity.ShopItem.ShopGoods;
+using AutoMapper;
 
 namespace ShopWebAPI.Controllers
 {
     public class GoodsController : ApiController
     {
+        GoodsServices services;
+        IMapper mapper;
+
+        public GoodsController (GoodsServices services)
+        {
+            this.services = services;
+        }
+        public GoodsController ()
+        { }
+
         [HttpPost]
         [Route ("api/Goods/Add")]
         public IHttpActionResult Add ([FromBody]AddModel model)
         {
             if (ModelState.IsValid)
             {
-                
-                return Ok ("");
-            }
+                var _goods = mapper.Map<Goods> (model);
+                bool result = services.Add (_goods);
+
+                if (result)
+                    return Ok ();
+            } else
+                return BadRequest (ModelState);
+
             return BadRequest ();
         }
 
         [HttpGet]
-        [Route ("api/Goods/Remove")]
-        public IHttpActionResult Remove ([FromUri]RemoveModel model)
+        [Route ("api/Goods/Remove/{id}")]
+        public IHttpActionResult Remove (int id)
         {
-            if (ModelState.IsValid)
+            services.Del (services.Get (id));
+
+            return Ok ();
+        }
+
+        [HttpGet]
+        [Route ("api/Goods/Get/{id}")]
+        public IHttpActionResult Get (int id)
+        {
+            var goods = services.Get (id);
+            if (goods != null)
             {
-                return Ok ("");
+                Goods _goods = mapper.Map<Goods> (goods);
+                return Ok (_goods);
             }
-            return BadRequest ();
+
+            return NotFound ();
+        }
+
+        [HttpGet]
+        [Route ("api/Goods/GetAll")]
+        public IHttpActionResult GetAll ()
+        {
+            var goods = services.GetAll ();
+
+            if (goods.Count () == 0)
+                return NotFound ();
+
+            var _goods = mapper.Map<IEnumerable<Goods>> (goods);
+
+            return Ok (_goods);
         }
     }
 }
--- ShopWebAPI/Models/Goods/AddModel.cs
+++ ShopWebAPI/Models/Goods/AddModel.cs
@@ -2,10 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace ShopWebAPI.Models.Goods
 {
     public class AddModel
     {
+        public int GoodsId { get; set; }
+
+        [Required]
+        [StringLength (250, MinimumLength = 5, ErrorMessage = "Name must be between 5 and 250 characters")]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength (1000, MinimumLength = 10, ErrorMessage = "Description must be between 10 and 1000 characters")]
+        public string Description { get; set; }
+
+        [Required]
+        [Range (double.Epsilon, double.MaxValue, ErrorMessage = "Must be positive value")]
+        public double Price { get; set; }
     }
 }

af64c19a9faecfd82f1f2a873650bc158ceef595
Derfly <kursDerfly@nonextrain.pp.ua>
2020-06-02 17:16:53 +0300

    + Added Statistic controller

--- ShopWebAPI/Controllers/StatisticController.cs
+++ ShopWebAPI/Controllers/StatisticController.cs
@@ -2,12 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Mvc;
 using System.Web.Http;
+using ShopBLL.Services.ServicesEntity;
+using AutoMapper;
+using ShopBLL.Entity.ShopItem.ShopUtility;
 
 namespace ShopWebAPI.Controllers
 {
     public class StatisticController : ApiController
     {
+        StatisticService services;
+        IMapper mapper;
+
+        public StatisticController (StatisticService services)
+        {
+            this.services = services;
+        }
+        public StatisticController ()
+        { }
+
+        [HttpGet]
+        [Route ("api/Statistic")]
+        public IHttpActionResult Get ()
+        {
+            return Ok (services.Get ());
+        }
     }
 }

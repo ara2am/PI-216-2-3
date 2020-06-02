a6dfc7d2bc51d4821d9376d8849c6e5e0eca3565
Derfly <kursDerfly@nonextrain.pp.ua>
2020-06-02 17:14:06 +0300

    + Added Statistic

---
+++ ShopBLL/Entity/Statistic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopBLL.Entity
+{
+    public class Statistic
+    {
+        public int TotalCountOfUsers { get; set; }
+        public int TotalCountOfGoods { get; set; }
+        public int TotalCountOfGoodsTypes { get; set; }
+        public int TotalCountOfOrders { get; set; }
+    }
+}
